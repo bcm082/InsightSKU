5fed1a68a6244bf8778a7e3639b1be009aecf93a
Bruno Martins <gz4256@wayne.edu>
2023-12-14 11:00:17 -0500

    Models added

--- insightsku/insightskuapp/models.py
+++ insightsku/insightskuapp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    client_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.client_name
+
+
+class User(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)  # Consider using Django's built-in User model for authentication
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Product(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    sKU = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_type = models.CharField(max_length=100, blank=True)
+    tags = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    image_url = models.URLField(blank=True)
+    quantity = models.IntegerField()
+    weight = models.DecimalField(max_digits=10, decimal_places=3)
+    weight_unit = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+
+
+class Content(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField()
+    bullet_points = models.TextField(blank=True)
+    search_terms = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Content for {self.product.product_name}"
+
+
+class ReturnsData(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    qty_sold = models.IntegerField()
+    qty_returned = models.IntegerField()
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Returns Data for {self.product.product_name} - {self.year}/{self.month}"
