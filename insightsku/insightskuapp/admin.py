a84bbb1b95f4d86cbc0da598dae8e4b3925ab601
Bruno Martins <bcmjj8200@gmail.com>
2023-12-20 12:50:27 -0500

    Update models to a simpler setup

--- insightsku/insightskuapp/admin.py
+++ insightsku/insightskuapp/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from .models import Client, UserProfile, Product, Content, ReturnsData, ProductType, Tag, ImageDetail
+from .models import Client, UserProfile, Product, Tag
 
 admin.site.site_header = 'InsightSKU Admin'
 admin.site.register(Client)
 admin.site.register(UserProfile)
 admin.site.register(Product)
-admin.site.register(Content)
-admin.site.register(ReturnsData)
-admin.site.register(ProductType)
 admin.site.register(Tag)
-admin.site.register(ImageDetail)
+
 
 
--- insightsku/insightskuapp/models.py
+++ insightsku/insightskuapp/models.py
@@ -17,79 +17,26 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
-class ProductType(models.Model):
-    type_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.type_name
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
-class ImageDetail(models.Model):
-    detail = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.detail
-
-
 class Product(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
     sku = models.CharField(max_length=40)
-    product_name = models.CharField(max_length=255)
-    brand = models.CharField(max_length=100, blank=True)
-    product_type = models.ManyToManyField(ProductType, blank=True)
+    asin = models.CharField(max_length=40, blank=True)
+    title = models.CharField(max_length=255)
+    main_image = models.URLField()
+    images_url = models.URLField()
+    videos_url = models.URLField()
+    a_plus_content_url = models.URLField()
     tags = models.ManyToManyField(Tag, blank=True)
-    location = models.CharField(max_length=255, blank=True)
-    image_url = models.URLField(blank=True)
-    image_location = models.URLField(blank=True)
-    image_details = models.ManyToManyField(ImageDetail, blank=True)
-    quantity = models.IntegerField()
-    weight = models.DecimalField(max_digits=10, decimal_places=3)
-    weight_unit = models.CharField(max_length=50, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    notes = models.TextField(blank=True)
+    date_listed = models.DateTimeField(null=True, blank=True)  # Automatically sets the date when the product is created
 
     def __str__(self):
-        return self.product_name
-    
-
-
-class Price(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    marketplace = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.marketplace} - {self.price}"
-
-
-class Content(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    description = models.TextField()
-    bullet_points = models.TextField(blank=True)
-    search_terms = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Content for {self.product.product_name}"
-
-
-class ReturnsData(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    year = models.IntegerField()
-    month = models.IntegerField()
-    qty_sold = models.IntegerField()
-    qty_returned = models.IntegerField()
-    percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    reason = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Returns Data for {self.product.product_name} - {self.year}/{self.month}"
+        return self.title
