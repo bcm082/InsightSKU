96276e170ec7bfe9220bc9ed5cf9a26c2cec6738
Bruno Martins <bcmjj8200@gmail.com>
2024-02-02 16:34:52 -0500

    add_products page

--- insightsku/insightskuapp/forms.py
+++ insightsku/insightskuapp/forms.py
@@ -4,9 +4,7 @@ from .models import Product
 class ClientNameForm(forms.Form):
     client_name = forms.CharField(label='Client Name', max_length=255)
 
-
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ['client']
-
